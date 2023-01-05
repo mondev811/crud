0cb14e35426145573a72cbd48dab493c974acbb1
Monica Rivera Pun <mondev811@gmail.com>
2023-01-04 23:37:28 -0800

    Add stack navigator

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import 'react-native-gesture-handler';
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Start} from './src/views/Start';
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Start" component={Start} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
