220cd8f528ce76de11ae993b9c404d791c9f2492
Monica Rivera Pun <mondev811@gmail.com>
2023-01-05 23:28:05 -0800

    Rename Start to Home

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {Start} from './src/views/Start';
+import {Home} from './src/views/Home';
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Start" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-export const Start = () => {
+export const Home = () => {
   return (
     <View>
       <Text>Start</Text>
