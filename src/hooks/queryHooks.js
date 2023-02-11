211401578cf64538b1f77ae4ea505b158eacb996
Monica Rivera Pun <mondev811@gmail.com>
2023-02-11 00:38:28 -0800

    Fix fetching bugs

--- src/hooks/queryHooks.js
+++ src/hooks/queryHooks.js
@@ -2,14 +2,21 @@ import {useQueryClient, useMutation, useQuery} from '@tanstack/react-query';
 import axios from 'axios';
 import * as CONSTANTS from '../helpers/constants';
 
+const apiClient = axios.create({
+  baseURL: CONSTANTS.SERVER_URL,
+  'Content-type': 'application/json',
+});
+
 export const useClientsData = () => {
-  const clientsQuery = useQuery(
-    ['clients'],
-    () => axios.get(`${CONSTANTS.SERVER_URL}/clients`),
-    {
+  const clientsQuery = useQuery({
+    queryKey: ['clients'],
+    queryFn: async () => {
+      return await apiClient.get('/clients');
+    },
+    options: {
       staleTime: 1000 * 60 * 3, // three minutes
     },
-  );
+  });
   return clientsQuery;
 };
 
@@ -31,7 +38,7 @@ export const useUpdateClient = () => {
   const queryClient = useQueryClient();
   const updateMutation = useMutation({
     mutationFn: client => {
-      return axios.put(`${CONSTANTS.SERVER_URL}/clients`, client);
+      return axios.put(`${CONSTANTS.SERVER_URL}/clients/${client.id}`, client);
     },
     onSuccess: () => {
       queryClient.invalidateQueries({queryKey: ['clients']});
